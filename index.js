8f842b30a367e5f7c37f46c5a646ace3d04add51
Alexandr <alexandr.kizilow@gmail.com>
2018-11-29 16:39:26 +0300

    change delete arrayUserId, add setUserId

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 let express = require('express')
 let requestBody = require('./requestBody')
+let fetch = require('node-fetch')
 require('dotenv').config()
 
 let app = express()
 
-let arrayUserId = []
+let setUserId = new Set()
 
 app.get('/', (req, res) => {
   let userId = req.query.id
 
-  for (i=0; i<=arrayUserId.length; i++) {
-    if (userId == arrayUserId[i]){
-      fetch(process.env.RESP_URL, {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(requestBody)
-      })
-      break
-    } else {
-      arrayUserId.push(userId)
-      res.json({text: 'Вам необходимо нажать на кнопку номер 4-2'})
-      break
-    }
+  if (setUserId.has(userId)){
+    fetch(process.env.RESP_URL, {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(requestBody)
+    })
+      
+  } else {
+    setUserId.add(userId)
   }
 
 })
